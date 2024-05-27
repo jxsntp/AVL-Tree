01b60851878576a7bd8fa2ae1715937f4fe08948
samuel <samuel.sukatja@gmail.com>
2024-05-27 22:45:51 +0800

    Tes 1

--- AVL tree.cpp
+++ AVL tree.cpp
@@ -9,7 +9,7 @@ public:
     int balance;
     AVLnode *left, *right, *parent;
  
-    AVLnode(T k, AVLnode p) : key(k), balance(0), parent(p), left(NULL), right(NULL) {}
+    AVLnode(T k, AVLnode *p) : key(k), balance(0), parent(p), left(NULL), right(NULL) {}
  
     ~AVLnode() {
         delete left;
